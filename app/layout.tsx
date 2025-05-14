63736fe0cbe6be3875d70c39e66a96c3ff08c71b
Nantachai Sangadsri <bz.nantachai@gmail.com>
2025-05-14 13:03:36 +0700

    feat: add PWA support with metadata and manifest configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,52 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import { Styledroot } from "./Styledroot";
 import "./globals.css";
 
+const APP_NAME = "Park Note App";
+const APP_DEFAULT_TITLE = "Park Note App";
+const APP_TITLE_TEMPLATE = "%s - Park Note App";
+const APP_DESCRIPTION =
+  "A simple app to help me remember where me parked my car ";
+
 export const metadata: Metadata = {
-  title: "Park Note App",
-  description: "A simple app to help me remember where me parked my car ",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
